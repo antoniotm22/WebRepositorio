efcdc89b8e53267f9179a00852dd81369a934cae
Antonio <Antonio@DESKTOP-SC2IMIF>
2020-09-29 15:34:49 +0200

    Actualizacion ejercicio 3 Bloque 3 Capitulo 2

--- RepositorioCompartido/src/paqueteAntonio/capitulo2/Bloque3.java
+++ RepositorioCompartido/src/paqueteAntonio/capitulo2/Bloque3.java
@@ -52,6 +52,7 @@ public class Bloque3 {
 	private static void ejercicio3Bloque3() {
 		//Voy a hacer un menu, con switch, para que cuando diga la opcion se ejecute esa parte.
 //
+//	MENU EN CONSOLA:		
 //		System.out.println("Menú:"+"\n"+
 //						"1- Calculo de Hipotenusa"+"\n"+
 //						"2- Calculo de la superficie de una circunferencia"+"\n"+
@@ -67,37 +68,58 @@ public class Bloque3 {
 				"3- Cálculo del perímetro de una circunferencia."+"\n"+
 				"4- Cálculo del área de un rectángulo."+"\n"+
 				"5- Cálculo del área de un triángulo."+"\n"+
-				"6- Salir de aplicación");
+				"0- Salir de aplicación");
 		int opcion= Integer.parseInt(str);
 		
-	
+		
+		 	if (opcion < 0 || opcion > 5) {
+			System.out.println("Opci�n no v�lida. Vuelva a escoger");
+		 	}else if (opcion !=0){
+		 	
+		 		switch(opcion) {
+				
+				case 1:
+					float cateto1=Float.parseFloat(JOptionPane.showInputDialog("Introduce los datos de un cateto"));
+					float cateto2=Float.parseFloat(JOptionPane.showInputDialog("Introduce los datos de un cateto"));
+					float hipotenusa;
+					
+					hipotenusa= (float) Math.sqrt(Math.pow(cateto1,2) + Math.pow(cateto2, 2));
+					System.out.println("La hipotenusa es: "+hipotenusa);
+					
+					break;
+				case 2:
+					float radio=Float.parseFloat(JOptionPane.showInputDialog("Introduce los datos del radio"));
+					double superficie;
+					
+					superficie = (Math.PI*(Math.pow(radio, 2)));
+					System.out.println("La superficie del circulo es: "+ superficie);
+					
+					break;
+				case 3:
+					
+					float radioC=Float.parseFloat(JOptionPane.showInputDialog("Introduce los datos del radio"));
+					double perimetro;
+					
+					perimetro=(2 * Math.PI * radioC);
+					System.out.println("El perimetro es: "+ perimetro);
+					
+					break;
+				case 4:
+					
+					break;
+				case 5:
+					
+					break;
+				case 6:
+					
+					break;
+				
+				default:
+					System.exit(opcion);
+				}
+		 	}
 
-		switch(opcion) {
-		
-		case 1:
-			
-			
-			
-			break;
-		case 2:
-			
-			break;
-		case 3:
-			
-			break;
-		case 4:
-			
-			break;
-		case 5:
-			
-			break;
-		case 6:
-			
-			break;
-		
-		default:
-			System.exit(opcion);
-		}
+	
 	}
 	
 	
