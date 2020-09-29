52312ecc3c78b062b03ab9753c676da30bdd6308
ASUS <ASUS@192.168.15.18>
2020-09-29 12:49:09 +0200

    ey

--- RepositorioCompartido/src/paqueteAlmudena/kk.java
+++ RepositorioCompartido/src/paqueteAlmudena/kk.java
@@ -5,6 +5,11 @@ public class kk {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//creada para poder crear el paquete
+		System.out.println("Numeros Bajos = " + numeroBajos +  '\n' + 
+				   "Numeros Medios = " + numeroMedios +  '\n' + 
+				   "Numeros Grandes = " + numeroGrandes +  '\n'
+				   
+		);
 	}
 
 }
