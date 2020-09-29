8f256ae73edb4cc1e17e4132d835a7236fd65b26
Rubén <Rubn@192.168.14.87>
2020-09-29 12:55:28 +0200

    Hola

---
+++ RepositorioCompartido/src/paqueteRuben/Ejercicios/Ejercicio5.java
@@ -0,0 +1,34 @@
+package paqueteRuben.Ejercicios;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+
+		
+		/* Realiza un programa en C que pida dos variables al usuario, 
+		intercambie los valores de las dos variables y las muestre en pantalla 
+		ANTES y DESPUéS del intercambio de valores.*/
+		
+		String cad;
+		cad = JOptionPane.showInputDialog("Introduce un número: ");
+		float num1 = Float.parseFloat(cad);
+		cad = JOptionPane.showInputDialog("Introduce otro número: ");
+		float num2 = Float.parseFloat(cad);
+		
+		System.out.println("El primer número es: " + num1);
+		System.out.println("El segundo número es: " + num2);
+		
+		float aux = num1;
+		num1 = num2;
+		num2 = aux;
+		
+		System.out.println("Se han cambiado los número");
+		System.out.println("El primer número es: " + num1);
+		System.out.println("El segundo número es: " + num2);
+	}
+
+	}
+
+}
