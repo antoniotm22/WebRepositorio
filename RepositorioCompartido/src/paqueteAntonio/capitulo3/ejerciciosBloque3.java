9f511782387a3be66599e3eccdded97bf6a408a5
Antonio <Antonio@DESKTOP-SC2IMIF>
2020-09-30 20:53:08 +0200

    actualizacion

--- RepositorioCompartido/src/paqueteAntonio/capitulo3/ejerciciosBloque3.java
+++ RepositorioCompartido/src/paqueteAntonio/capitulo3/ejerciciosBloque3.java
@@ -8,7 +8,7 @@ public class ejerciciosBloque3 {
 	 * 
 	 * 1º.- Realiza un ejercicio que pida números al usuario. El programa debe detenerse cuando el usuario introduzca el número 0 (cero),
 	 *  que no debe tenerse en cuenta para ninguna operación aritmética, simplemente para salir de la aplicación.
-	 *  Cuando el programa haya terminado, se debe sacar en pantalla el valor de la suma de todos los números.
+	 *  Cuando el programa haya terminado, se debe sacar en pantalla el valor de la suma de todos los números .
 	 */
 	private static void ejercicio1() {
 		
