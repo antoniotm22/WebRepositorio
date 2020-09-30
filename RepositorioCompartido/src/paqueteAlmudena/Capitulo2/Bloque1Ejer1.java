12a8fddd8b11c667d000526ab63661e8b6c6d04f
ASUS <ASUS@192.168.1.103>
2020-09-30 10:54:06 +0200

    Capi2

---
+++ RepositorioCompartido/src/paqueteAlmudena/Capitulo2/Bloque1Ejer1.java
@@ -0,0 +1,32 @@
+package paqueteAlmudena.Capitulo2;
+
+import javax.swing.JOptionPane;
+
+public class Bloque1Ejer1 {
+
+	public static void main(String[] args) {
+		//Realiza un ejercicio que pida dos variables al usuario, tras lo cual, muestre en pantalla
+		//el número mayor de los dos introducidos.
+		
+		//declaramos las variables
+		double var1;
+		double var2;
+		String srt;
+		
+		//pedimos los datos
+		srt = JOptionPane.showInputDialog("Introduce el primer valor");
+		var1 = Double.parseDouble(srt);
+		
+		srt = JOptionPane.showInputDialog("Introduce el segundo valor");
+		var2 = Double.parseDouble(srt);
+		
+		//comparamos valores
+		if (var1>var2) {
+			System.out.println("El mayor es: " + var1);
+		}else {
+			System.out.println("El mayor es: " + var2);
+		}
+
+	}
+
+}
