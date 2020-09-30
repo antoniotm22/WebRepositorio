10c0533a0c5c87b7dde6c46dcb66e1a74da2e165
Antonio Jesús Trujillo <55539288+antoniotm22@users.noreply.github.com>
2020-09-30 09:11:44 +0200

    Bloque 1 capitulo 3 ejercicios

--- RepositorioCompartido/src/paqueteAntonio/capitulo3/ejerciciosBloque1.java
+++ RepositorioCompartido/src/paqueteAntonio/capitulo3/ejerciciosBloque1.java
@@ -4,15 +4,17 @@ import javax.swing.JOptionPane;
 
 public class ejerciciosBloque1 {
 	
+	
+	
 	/**
-	 * 
+	 * Método que contiene el ejercicio 1 del bloque 1 del capitulo 1
 	 */
 	private static void ejercicio1() {
 		
 		int num;
 		int suma=0;
 		
-		 
+		 //El bucle se recorre 4 veces porque asi lo hemos indicado.
 		for (int i = 0; i < 4; i++) {
 			
 			num=Integer.parseInt( JOptionPane.showInputDialog("Introduce numero"));
@@ -25,45 +27,65 @@ public class ejerciciosBloque1 {
 		System.out.println("La suma de los numeros mayores de 10 es: "+suma);
 	}
 	
+	
+	
+	
 	/**
-	 * 
+	 * Método que contiene el ejercicio 2 del bloque 1 del capitulo 3
 	 */
 	private static void ejercicio2() {
 		int num;
 		int suma=0;
 		
-	
+		//Usuario indica cuantas veces se ejecuta el bucle
 		int repeticiones=Integer.parseInt( JOptionPane.showInputDialog("Introduce numero de veces a ejecutar"));
 		 
 		for (int i = 0; i < repeticiones; i++) {
-			
+			boolean esValorPositivo=false;
 			num=Integer.parseInt( JOptionPane.showInputDialog("Introduce numero"));
 			
-			
+			//Condicion, si el numero es mayor de 10 se sumará
 			if(num>10) {
 				suma+= num;
 			}
 		}
+		//Mostramos en pantalla el resultado
 		System.out.println("La suma de los numeros mayores de 10 es: "+suma);
 	}
 	
+	
+	
+	
+	/**
+	 * Método que contiene el ejercicio 3 del bloque 1 del capítulo 3
+	 */
 	private static void ejercicio3() {
-		boolean esValorPositivo=true;
+		
+		//Declaracion de variables
 		int num;
 		int contadorNegativo=0;
 		int contadorPositivo=0;
 		
+		//El usuario indica las veces que se va a recorrer el bucle
 		int repeticiones=Integer.parseInt( JOptionPane.showInputDialog("Introduce numero de veces a ejecutar"));
-	
+	 
 		
 			for (int i = 0; i < repeticiones; i++) {
-					int valor=Integer.parseInt( JOptionPane.showInputDialog("Introduce numero de veces a ejecutar"));	
+						
 					num=Integer.parseInt( JOptionPane.showInputDialog("Introduce numero"));
 						
-						if(esValorPositivo==true) {
-							
+						//si es mayor de 0 es positivo,por lo tanto entra en la condicion
+						if(num>=0) {
+							//contadorPositivo=contadorPositivo+1;
+							//Sumamos 1 al contador positivo
+							contadorPositivo++;
+							System.out.println("Hay: "+contadorPositivo+" números positivos" );
 						}
-						
+						else {
+							//sumamos 1 al contador negativo
+							contadorNegativo++;
+							System.out.println("Hay: "+contadorNegativo+" números negativos" );
+						}
 						
 					}
 	}
@@ -74,8 +96,8 @@ public class ejerciciosBloque1 {
 	 */
 	public static void main(String[] args) {
 		//ejercicio1();
-		ejercicio2();
-
+		//ejercicio2();
+		//ejercicio3();
 	}
 
 }
