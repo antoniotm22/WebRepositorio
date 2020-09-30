b21fb7f9d682540a6fcf84d203ce16b2e5ef7ffe
Almudena Delgado <denaa188@gmail.com>
2020-09-30 18:18:43 +0200

    bloque 3

---
+++ RepositorioCompartido/src/paqueteAlmudena/Capitulo2/Bloque3Ejer2.java
@@ -0,0 +1,12 @@
+package paqueteAlmudena.Capitulo2;
+
+public class Bloque3Ejer2 {
+
+	public static void main(String[] args) {
+		//Realiza una calculadora avanzada, pero con la particularidad de que las operaciones
+		//deben ser raices, potencias y el módulo de la división.
+
+		
+	}
+
+}
