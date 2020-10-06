41a0fbac90d24c83e7d006e32b194b5aad6cc612
Almudena Delgado <denaa188@gmail.com>
2020-10-06 12:17:32 +0200

    EDD EJER5

--- RepositorioCompartido/src/paqueteAlmudena/Capitulo3/Bloque3/Ejer5.java
+++ RepositorioCompartido/src/paqueteAlmudena/Capitulo3/Bloque3/Ejer5.java
@@ -23,11 +23,11 @@ public class Ejer5 {
 				primeraVez = false;
 			}
 			
-			if (num > mayor) {
+			if (num > mayor ) {
 				mayor = num;
 			}
 
-			if (num < menor) {
+			if (num < menor && num != 0) {
 				menor = num;
 			}
 			
