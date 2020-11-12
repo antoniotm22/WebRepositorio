3c0984eb33c867661202cb680ac6c8175df5cd32
Zulit <49183495+zulit12@users.noreply.github.com>
2020-11-12 14:52:57 +0100

    ADD metodos

--- RepositorioCompartido/src/paqueteZulit/Ejercicios/Metodos.java
+++ RepositorioCompartido/src/paqueteZulit/Ejercicios/Metodos.java
@@ -4,51 +4,76 @@ import javax.swing.JOptionPane;
 
 public class Metodos {
 
-
+	/**
+	 * 
+	 * @return
+	 */
 	public static boolean metodoMedia4Num () {
-
+		//Pedir al usuario los valores y asiganr dichos valores a sus variables
 		int num1 = Integer.parseInt(JOptionPane.showInputDialog("Inserte el primer valor"));
 		int num2 = Integer.parseInt(JOptionPane.showInputDialog("Inserte el segundo valor"));
 		int num3 = Integer.parseInt(JOptionPane.showInputDialog("Inserte el tercer valor"));
 		float num4 = Float.parseFloat(JOptionPane.showInputDialog("Inserte el cuarto valor"));
 		
+		//Calcular media de ttodos los numeros
 		float media = (float) ((num1 + num2 + num3 + num4)/4);
 		
+		//Mostrar en pantalla
 		System.out.println("La media de los valores asignados es: " + media);
 		
 		return true;
 	}
-	
+
+	/**
+	 * 
+	 * @param numMIN
+	 * @param numMAX
+	 * @return
+	 */
 	public static int obtenerNumeroDeUsuarioEntreMinimoYMaximo (int numMIN , int numMAX) {
-		
+		//Crear variable 
 		int numero;
 		
-		do {
+		//Comprobación y condición a la cual sometemos el numero pedido al usuario
+		do {			
 			String str = JOptionPane.showInputDialog("Inserte el valor a comprobar");
 			numero = Integer.parseInt(str);
 		} while (!(numero > numMIN && numero < numMAX));
 		
+		//Mostrar el numero
 		System.out.println("El numero introducido es correcto: " + numero);
 		
 		return numero;
 		
 	}
 	
+	/**
+	 * 
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
 	public static boolean maximo (int num1 , int num2) {
 		
 		int maximo;
-		
+		//Comprobación de los valores y asignación a la variable maximo
 		if (num1 > num2) {
 			maximo = num1;
 		}else {
 			maximo = num2;
 		}
-		
+		//Mostrar el valor maximo 
 		System.out.println("El numero de mayor valor introducido " + maximo);
 		
 		return true;
 	}
-
+	
+	/**
+	 * 
+	 * @param limInf
+	 * @param limSup
+	 * @return
+	 */
 	public static boolean numeroImparEntreLimites ( int limInf ,int limSup) {
 		
 		int numAzar;
@@ -63,6 +88,12 @@ public class Metodos {
 		return true;
 	}
 	
+	/**
+	 * 
+	 * @param limInf
+	 * @param limSup
+	 * @return
+	 */
 	public static boolean getStringFromEntero (int limInf, int limSup) {
 		
 		int num = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero del 1 al 10"));
